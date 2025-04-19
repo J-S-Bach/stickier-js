97f3685a7d84c9fcb63387e7485362e7c4a138bf
J-S-Bach <basti.kirner@yahoo.de>
2025-04-19 19:40:19 +0200

    added build flow for ts files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,92 +1,89 @@
-'use strict';
-
+"use strict";
 
 //
 // Gulp
 // --------------------------------------------------
 
-
-
 /*
  * Import gulp modules
  */
-import gulp from 'gulp';
-import del from 'del';
-import babel from 'gulp-babel';
-import rename from 'gulp-rename';
-import uglify from 'gulp-uglify';
-import size from 'gulp-size';
-import gzip from 'gulp-gzip';
-import browser_sync from 'browser-sync';
-
-
+import gulp from "gulp";
+import del from "del";
+import babel from "gulp-babel";
+import rename from "gulp-rename";
+import uglify from "gulp-uglify";
+import size from "gulp-size";
+import gzip from "gulp-gzip";
+import ts from "gulp-typescript";
+import browser_sync from "browser-sync";
+import merge from "merge2";
 
 /*
  * Create browserSync instance
  */
 const browserSync = browser_sync.create();
 
-
-
 /*
  * Clean dist directory
  */
-const clean = () => del([ './dist' ]);
+const clean = () => del(["./dist"]);
 export { clean };
 
-
+/*
+ * get ts configs of this project
+ */
+const tsProject = ts.createProject("tsconfig.json", { declaration: true });
 
 /*
- * Compile js
+ * Compile ts
  */
-export function js() {
-  return gulp.src('./src/sticky.js')
-    .pipe(babel())
-    .pipe(rename('sticky.compile.js'))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(size({ title: 'compiled:' }))
-    .pipe(uglify())
-    .pipe(size({ title: 'minified:' }))
-    .pipe(rename('sticky.min.js'))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(gzip())
-    .pipe(size({ title: 'gzipped:' }))
-    .pipe(gulp.dest('./dist/'));
+export function compileTs() {
+  const tsResults = gulp.src("./src/sticky.ts").pipe(tsProject());
+
+  return merge([
+    tsResults.dts.pipe(gulp.dest("./dist/")),
+    tsResults.js
+      .pipe(babel())
+      .pipe(rename("sticky.compile.js"))
+      .pipe(gulp.dest("./dist/"))
+      .pipe(size({ title: "compiled:" }))
+      .pipe(uglify())
+      .pipe(size({ title: "minified:" }))
+      .pipe(rename("sticky.min.js"))
+      .pipe(gulp.dest("./dist/"))
+      .pipe(gzip())
+      .pipe(size({ title: "gzipped:" }))
+      .pipe(gulp.dest("./dist/")),
+  ]);
 }
-
-
 
 /*
  * Serve
  */
 export function serve() {
-  gulp.watch('./src/*.js', gulp.series(js))
+  gulp.watch("./src/*.js", gulp.series(compileTs));
 
-  gulp.watch('./demo/*.html').on('change', browserSync.reload);
-  gulp.watch('./dist/*.js').on('change', browserSync.reload);
+  gulp.watch("./demo/*.html").on("change", browserSync.reload);
+  gulp.watch("./dist/*.js").on("change", browserSync.reload);
 
   return browserSync.init({
     server: {
-      baseDir: './',
+      baseDir: "./",
       directory: true,
     },
 
-    startPath: '/demo/index.html',
+    startPath: "/demo/index.html",
   });
 }
-
-
 
 /*
  * Builder
  */
-const build = gulp.series(clean, js);
+const build = gulp.series(clean, compileTs);
 export { build };
 
-const server = gulp.series(clean, js, serve);
+const server = gulp.series(clean, compileTs, serve);
 export { server };
-
-
 
 /*
  * Export a default task
